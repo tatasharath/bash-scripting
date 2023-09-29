bfeb5f4723aa44d4af385cfa9dff65f378641b24
Tata Sarath <you@example.com>
2023-09-30 00:02:48 +0530

    Trying colors

--- 03-colors.sh
+++ 03-colors.sh
@@ -13,13 +13,13 @@
 # Syntax To Print Color Text Is :
 # Ex : 
 # echo -e "\e[COLORCODEm  Your Msg To Be Printed In Color \e[0m"
-# echo -e "\e[31m My name is Sarath \e[0m"
+echo -e "\e[31m My name is Sarath \e[0m"
 
 # To print something with background + foreGround color , here is the syntax: 
 # Ex : 
 #   echo -e "\e[backGrouCode;foreGroudCodem your Message \e[0m"
 
-echo -e "\e[43;30 my wife name is chakrini \e[0m"
+echo -e "\e[43;30m my wife name is chakrini \e[0m"
 
 echo -e "\e[33m I am printing YELLOW Color  \e[0m"
 echo -e "\e[32m I am printing GREEN Color   \e[0m"
