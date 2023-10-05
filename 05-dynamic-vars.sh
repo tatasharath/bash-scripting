85b66690a6bd97ed7338118e0c2903ed35c5abaa
Tata Sarath <you@example.com>
2023-10-05 23:42:41 +0530

    Gave colors

--- 05-dynamic-vars.sh
+++ 05-dynamic-vars.sh
@@ -4,4 +4,4 @@ DATE="$(date +%F)"
 
 echo -e "Todays date is \e[34m $DATE \e[0m"
 SESSIONS_COUNT="$(who | wc -l)"
-echo -e "Total Number of Active Sessions \e [32m $SESSIONS_COUNT \e[0m"
+echo -e "Total Number of Active Sessions \e[32m $SESSIONS_COUNT \e[0m"
