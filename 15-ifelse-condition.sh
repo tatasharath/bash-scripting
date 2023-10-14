c994a01fcfbe2849fe5eb97a5ade6d20b299e38d
tatasharath <tatasharath@gmail.com>
2023-10-15 00:46:54 +0530

    ifelse condition

--- 15-ifelse-condition.sh
+++ 15-ifelse-condition.sh
@@ -9,13 +9,12 @@ echo -e "\e[32m Starting my demo on if \e[0m"
 exit 0
 
 else if [ "$ACTION" == "stop" ]; then
-echo "\e[31m Stopping my demo on if \e[0m"
+echo -e "\e[31m Stopping my demo on if \e[0m"
 
 else if [ "$ACTION" == "restart" ]; then
-echo "\e[33m Restarting my demo on if \e[0m"
+echo -e "\e[33m Restarting my demo on if \e[0m"
 
 else
  echo -e "\e[31m condition is false \e[0m"
-
 
 fi
