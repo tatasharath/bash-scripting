08c0940e549d10c210e2e36013aa40614c0137d5
Tata Sarath <you@example.com>
2023-10-14 22:53:20 +0530

    conditions

--- 12-conditions.sh
+++ 12-conditions.sh
@@ -1,4 +1,33 @@
 #!/bin/bash
 
 
-# Conditions help us only if the Some-Factor is true and if that factor is a fals that something won't be executed.
+# Conditons helps us to execute something only if SOME-Factor is true and if that factor is false then that something won't be executed.
+
+
+# Syntax Of CASE 
+# case $var in 
+#     opt1)  commands-x ;; 
+#     opt2)  commands-y ;;
+# esac  
+
+ACTION=$1
+
+case $ACTION in 
+    start)
+        echo -e "\e[32m Starting Payment Service \e[0m"
+        exit 0
+        ;;
+    stop)
+        echo -e "\e[31m Stopping Payment Service \e[0m"
+        exit 1
+        ;;
+    restart)
+        echo -e "\e[33mRestarting Payment Service \e[0m"
+        exit 2
+        ;;
+    *)
+        echo -e "\e[35m Valid Options are start or stop or restart \e[0m"
+        echo -e "\e[33m Example Usage \e[0m :\n \t bash scriptName stop"
+        exit 3
+        ;;
+esac 
