e77dee210b7d54f8cde1e675360958be4b2925a2
tatasharath <tatasharath@gmail.com>
2023-10-15 00:45:56 +0530

    ifelse condition

--- 15-ifelse-condition.sh
+++ 15-ifelse-condition.sh
@@ -5,9 +5,17 @@ echo "demo on if and if-else and else-if usage"
 ACTION=$1
 
 if [ "$ACTION" == "start" ]; then
-echo -e "\e[32m starting my demo on if \e[0m"
+echo -e "\e[32m Starting my demo on if \e[0m"
 exit 0
+
+else if [ "$ACTION" == "stop" ]; then
+echo "\e[31m Stopping my demo on if \e[0m"
+
+else if [ "$ACTION" == "restart" ]; then
+echo "\e[33m Restarting my demo on if \e[0m"
 
 else
  echo -e "\e[31m condition is false \e[0m"
+
+
 fi
