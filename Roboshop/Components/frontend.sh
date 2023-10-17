9f8c14457f2c3edbc2f2a8bb826b9c634097b9c7
tatasharath <tatasharath@gmail.com>
2023-10-18 00:28:06 +0530

    ifwrapper.sh

--- Roboshop/Components/frontend.sh
+++ Roboshop/Components/frontend.sh
@@ -4,7 +4,7 @@ USER_ID=$(id -u)
 
 if [ $USER_ID -ne 0 ] ; then 
 
-echo -e "\e[31m to excute the command user should be a root user or a sudo \n \t Example: sudo bash wrapper.sh \e[0m"
+echo -e "\e[31m to excute the command user should be a root user or a sudo \e[0m \n \t Example: sudo bash wrapper.sh"
 
 exit 1
 fi
