5c0251161e2524a8e03d1e1cfe3359db7cf1ccee
tatasharath <tatasharath@gmail.com>
2023-10-18 00:50:27 +0530

    ifwrapper.sh

--- Roboshop/Components/frontend.sh
+++ Roboshop/Components/frontend.sh
@@ -2,19 +2,31 @@
 
 USER_ID=$(id -u)
 
-if [ $USER_ID -ne 0 ] ; then 
-
-echo -e "\e[31m to excute the command user should be a root user or a sudo \e[0m \n \t Example: sudo bash wrapper.sh"
-
-exit 1
-fi
-
-
+if [ $USER_ID -eq 0 ] ; then 
 
 echo -e "\e[32m Configuring frontend.....\e[0m" 
 
 echo "Installing Frontend :"
 
-
 yum install nginx -y &>> /tmp/frontend.log
+
+elif [ $? -eq 0 ]; then
+
+echo "successful installed"
+
+else
+echo -e "\e[31m to excute the command user should be a root user or a sudo \e[0m \n \t Example: sudo bash wrapper.sh"
+
+exit 1
+fi
+
+# if
+
+
+# echo -e "\e[32m Configuring frontend.....\e[0m" 
+
+# echo "Installing Frontend :"
+
+
+# yum install nginx -y &>> /tmp/frontend.log
 
