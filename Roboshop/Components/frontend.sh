032be4cbcfc748dddebd503555a4e298c18fc5bd
tatasharath <tatasharath@gmail.com>
2023-10-18 00:19:50 +0530

    ifwrapper.sh

--- Roboshop/Components/frontend.sh
+++ Roboshop/Components/frontend.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+USER_ID=$(id -u)
 
-echo "I am frontend"
+if
+USER_ID -ne 0 then 
+
+echo -e "[/31m to excute the command user should be a root user or a sudo /n /t Example: sudo bash wrapper.sh [0m"
+exit 1
+fi
+
+
+# echo "Configuring frontend"
+
+# yum unstall nginx -y
+
