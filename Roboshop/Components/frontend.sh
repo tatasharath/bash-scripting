e8b890d1141f89eac385d91792761a8225e353e2
tatasharath <tatasharath@gmail.com>
2023-10-20 00:39:41 +0530

    start nginx

--- Roboshop/Components/frontend.sh
+++ Roboshop/Components/frontend.sh
@@ -28,18 +28,12 @@ fi
 fi
 
 echo -n "Starting nginx:"
-
 systemctl enable nginx
-exit 2
-
 systemctl start nginx
 
 if [ $? -eq 0 ] ; then
-
 echo -e "\e[32m Successfully started \e[0m"
-
 else
 echo -e "\e[31m nginx not started \e[0m"
-
 fi
 
