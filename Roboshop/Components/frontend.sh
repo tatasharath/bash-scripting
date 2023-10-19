0e1c5bff282fd4ef3c5cb9ccf45cfab33232da50
tatasharath <tatasharath@gmail.com>
2023-10-20 00:00:22 +0530

    trying elsif

--- Roboshop/Components/frontend.sh
+++ Roboshop/Components/frontend.sh
@@ -36,7 +36,7 @@ if [ $USER_ID -ne 0 ] ; then
     echo -e "\e[31m Script is expected to executed by the root user or with a sudo privilege \e[0m \n \t Example: \n\t\t sudo bash wrapper.sh frontend"
     exit
      1
-else
+elif [$USER_ID -eq 0]; then
 echo -e "\e[32m Configuring frontend.....\e[0m" 
 
 echo -n "Installing Frontend :"
