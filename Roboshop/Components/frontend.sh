0a710b7d954e344dd983f1323d6ea3551ca01260
tatasharath <tatasharath@gmail.com>
2023-10-19 23:46:15 +0530

    trying elsif

--- Roboshop/Components/frontend.sh
+++ Roboshop/Components/frontend.sh
@@ -6,25 +6,28 @@ USER=$?
 
 if [ $USER_ID -ne 0 ] ; then 
 
+echo -e "\e[31m Not Installed \e[0m"
+
+echo -e "\e[33m To excute the command user should be a root user or a sudo \e[0m \n \t Example: sudo bash wrapper.sh"
+exit 1
+
+
+# echo $?
+
+elif [ $USER_ID  -eq 0 ] ; then
 echo -e "\e[32m Configuring frontend.....\e[0m" 
 echo -n "Installing Frontend :"
 
 yum install nginx -y &>> /tmp/frontend.log
 exit 0
-
-# echo $?
-
-elif [ $USER -eq 0 ] ; then
-
+if($? eq 0); then
 echo -e "\e[32m Successful installed \e[0m"
+fi
 
 # echo "Successful installed"
 
 else
-echo -e "\e[31m Not Installed \e[0m"
-
-echo -e "\e[33m To excute the command user should be a root user or a sudo \e[0m \n \t Example: sudo bash wrapper.sh"
-exit 1
+echo "userid doecnt match"
 
 fi
 
