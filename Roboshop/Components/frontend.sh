7756a7afa21de2a08d6892e2b866279d977445d1
tatasharath <tatasharath@gmail.com>
2023-10-26 00:11:45 +0530

    test

--- Roboshop/Components/frontend.sh
+++ Roboshop/Components/frontend.sh
@@ -52,25 +52,25 @@ Sarath
 # echo -e "\e[31m nginx not started \e[0m"
 # fi
 
-echo -n Downloading the front end component :
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+# echo -n Downloading the frontend component :
+# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
 
-Sarath
-# if [ $? -eq 0 ] ; then
-# echo -e "\e[32m Success \e[0m"
-# else
-# echo -e "\e[31m Failure \e[0m"
-# fi
+# Sarath
+# # if [ $? -eq 0 ] ; then
+# # echo -e "\e[32m Success \e[0m"
+# # else
+# # echo -e "\e[31m Failure \e[0m"
+# # fi
 
-echo -n Clean up of frontend : 
-cd /usr/share/nginix/html
+# echo -n Clean up of frontend : 
+# cd /usr/share/nginix/html
 
-rm -rf * &>> /tmp/frontend.log
-Sarath
-echo  -n Extracting Frontend :
+# rm -rf * &>> /tmp/frontend.log
+# Sarath
+# echo  -n Extracting Frontend :
 
-unzip /tmp/Frontend.zip  &>> /tmp/frontend.log
-Sarath
+# unzip /tmp/Frontend.zip  &>> /tmp/frontend.log
+# Sarath
 
 
 
