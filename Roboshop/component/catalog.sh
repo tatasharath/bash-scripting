26ab07f094c4e2a62bf935f5c4b49d82af614b05
tatasharath <tatasharath@gmail.com>
2023-10-27 23:58:06 +0530

    configuring catalog

--- Roboshop/component/catalog.sh
+++ Roboshop/component/catalog.sh
@@ -1,3 +1,67 @@
+
 #!/bin/bash
 
-echo "I am catalog"
+set -e
+stat(){
+    if [ $? -eq 0 ] ; then
+echo -e "\e[32m Success \e[0m"
+else
+echo -e "\e[31m Failure \e[0m"
+fi
+
+}
+
+USER_ID=$(id -u)
+COMPONENT=catalog
+LOGFILE=/tmp/frontend.log
+APPUSER=roboshop
+
+if [ $USER_ID -ne 0 ] ; then 
+
+echo -e "\e[31m Not Installed \e[0m"
+
+echo -e "\e[33m To excute the command user should be a root user or a sudo \e[0m \n \t Example: sudo bash wrapper.sh"
+exit 1
+
+elif [ $USER_ID -eq 0 ] ; then
+
+echo -e "\e[33m Configuring ${COMPONENT}.....\e[0m" 
+
+echo  -n Configuring ${COMPONENT} repo :
+curl --silent --location https://rpm.nodesource.com/setup_16.x | bash - &>> ${LOGFILE}
+stat
+
+echo -n Installing NodJS :
+yum install nodejs -y  &>> ${LOGFILE}
+stat
+
+fi
+
+echo -n Creating Application User Account
+useradd roboshop
+
+
+# echo -n Downloading ${COMPONENT} Schema :
+# curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+# stat
+
+# echo -n Extracting ${COMPONENT} Schema :
+# cd /tmp
+# unzip -o ${COMPONENT}.zip &>> ${LOGFILE}
+# stat
+
+# echo -n Injecting ${COMPONENT} Schema :
+# cd ${COMPONENT}-main
+# mongo < catalogue.js &>> ${LOGFILE}
+# mongo < users.js &>> ${LOGFILE}
+# stat
+
+# echo -e "\e[35m ${COMPONENT} Installation is compleated \e[0m \n"
+
+
+
+
+
+
+
+
--- Roboshop/component/mongodb.sh
+++ Roboshop/component/mongodb.sh
@@ -63,56 +63,7 @@ echo -e "\e[35m ${COMPONENT} Installation is compleated \e[0m \n"
 
 
 
-# echo -n "Installing nginx:"
 
-# yum install nginx -y &>> ${LOGFILE}
-
-# stat
-
-
-# fi
-
-# echo -n "Starting nginx:"
-
-# systemctl enable nginx
-
-# systemctl start nginx
-
-# stat
-
-# if [ $? -eq 0 ] ; then
-# echo -e "\e[32m Successfully started \e[0m"
-# else
-# echo -e "\e[31m nginx not started \e[0m"
-# fi
-
-# echo -n Downloading the ${COMPONENT} component :
-# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-
-# stat
-
-# echo -n Clean up of ${COMPONENT} : 
-
-# cd /usr/share/nginx/html
-
-# rm -rf * &>> ${LOGFILE}
-# stat
-
-# echo  -n Extracting ${COMPONENT} :
-
-# unzip /tmp/${COMPONENT}.zip  &>> ${LOGFILE}
-
-# echo -n Sorting the ${COMPONENT} files
-# mv ${COMPONENT}-main/* .    
-# mv static/* .             
-# rm -rf frontend-main README.md 
-# mv localhost.conf /etc/nginx/default.d/roboshop.conf
-# stat
-
-# echo -n Restarting ${COMPONENT}:
-# systemctl daemon-reload &>> ${LOGFILE}
-# systemctl restart nginx &>> ${LOGFILE}
-# stat
 
 
 
