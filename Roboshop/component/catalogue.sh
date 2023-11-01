7a0d50f68e7410e35c559a31eef723634eae529f
tatasharath <tatasharath@gmail.com>
2023-11-02 00:23:40 +0530

    Starting the Services

--- Roboshop/component/catalogue.sh
+++ Roboshop/component/catalogue.sh
@@ -66,7 +66,7 @@ npm install     &>> ${LOGFILE}
 stat
 
 echo -n "Configuring the ${COMPONENT} system file:"
-sed -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' home/${APPUSER}/${COMPONENT}/systemd.service
+sed -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/${APPUSER}/${COMPONENT}/systemd.service
 mv /home/${APPUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
 stat
 
