1307fce2338489b1da118c6a189f817d619d26cc
tatasharath <tatasharath@gmail.com>
2023-11-07 23:40:07 +0530

    Installing Redis

--- Roboshop/component/redis.sh
+++ Roboshop/component/redis.sh
@@ -43,7 +43,7 @@ stat
 echo -n Starting the ${COMPONENT} :
 systemctl daemon-reload &>> ${LOGFILE}
 systemctl enable ${COMPONENT} &>> ${LOGFILE}
-systemctl start ${COMPONENT} &>> ${LOGFILE}
+systemctl restart ${COMPONENT} &>> ${LOGFILE}
 stat
 
 
