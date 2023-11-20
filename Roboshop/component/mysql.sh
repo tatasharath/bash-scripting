29a96b29978d42712bfaef3088de7e5e01161788
tatasharath <tatasharath@gmail.com>
2023-11-21 01:11:21 +0530

    Mysql

--- Roboshop/component/mysql.sh
+++ Roboshop/component/mysql.sh
@@ -8,10 +8,10 @@ echo -e "\e[35m Configuring ${COMPONENT} ......! \e[0m \n"
 
 echo -n "Configuring ${COMPONENT} repo :"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo
-stat
+stat $?
 
 echo -n "Installing ${COMPONENT}  :"
-yum install mysql-community-server -y    &>>  ${LOGFILE}
+yum install mysql-community-server -y     &>>  ${LOGFILE}
 stat $?
 
 # echo -n "Starting ${COMPONENT}:" 
