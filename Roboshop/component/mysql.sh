ed7a9e78950e5e9e65ace1e46f719a461f56f5d2
tatasharath <tatasharath@gmail.com>
2023-11-21 00:56:14 +0530

    Mysql

--- Roboshop/component/mysql.sh
+++ Roboshop/component/mysql.sh
@@ -8,20 +8,20 @@ echo -e "\e[35m Configuring ${COMPONENT} ......! \e[0m \n"
 
 echo -n "Configuring ${COMPONENT} repo :"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo
-stat $?
+stat
 
 echo -n "Installing ${COMPONENT}  :"
 yum install mysql-community-server -y     &>>  ${LOGFILE}
-stat $?
+stat 
 
 echo -n "Starting ${COMPONENT}:" 
 systemctl enable mysqld   &>>  ${LOGFILE}
 systemctl start mysqld    &>>  ${LOGFILE}
-stat $?
+stat 
 
 echo -n "Extracting the default mysql root password :"
 DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk -F " " '{print $NF}')
-stat $? 
+stat  
 
 # This should happen only once and that too for the first time, when it runs for the second time, jobs fails.
 # We need to ensure that this runs only once.
@@ -30,30 +30,30 @@ echo "show databases;" | mysql -uroot -pRoboShop@1 &>>  ${LOGFILE}
 if [ $? -ne 0 ]; then 
     echo -n "Performing default password reset of root account:"
     echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1'" | mysql  --connect-expired-password -uroot -p$DEFAULT_ROOT_PASSWORD &>>  ${LOGFILE}
-    stat $?
+    stat
 fi 
 
 echo "show plugins;" | mysql -uroot -pRoboShop@1 | grep validate_password  &>>  ${LOGFILE}
 if [ $? -eq 0 ]; then 
     echo -n "Uninstalling Password-validate plugin :"
     echo "uninstall plugin validate_password" | mysql -uroot -pRoboShop@1 &>>  ${LOGFILE}
-    stat $?
+    stat
 fi 
 
 
 echo -n "Downloading the $COMPONENT schema:"
 curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
-stat $? 
+stat 
 
 echo -n "Extracting the $COMPONENT Schema:"
 cd /tmp  
 unzip -o /tmp/${COMPONENT}.zip   &>> $LOGFILE
-stat $? 
+stat
 
 echo -n "Injecting the schema:"
 cd ${COMPONENT}-main 
 mysql -u root -pRoboShop@1 <shipping.sql     &>>  ${LOGFILE} 
-stat $? 
+stat
 
 
 echo -e "\e[35m ${COMPONENT} Installation Is Completed \e[0m \n"
