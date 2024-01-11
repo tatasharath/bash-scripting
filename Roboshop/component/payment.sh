1503f4615b237db9af5dc957e22852bc8e7c24f2
tatasharath <tatasharath@gmail.com>
2024-01-11 22:27:33 +0530

    Payment

--- Roboshop/component/payment.sh
+++ Roboshop/component/payment.sh
@@ -3,7 +3,7 @@
 COMPONENT=payment
 
 # This is how we import the functions that are declared in a different file using source 
-source components/common.sh
+source component/common.sh
 PYTHON                       # calling nodejs function.
 
 echo -e "\n \e[35m ${COMPONENT} Installation Is Completed \e[0m \n"
