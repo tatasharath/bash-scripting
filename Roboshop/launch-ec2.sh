dd257aa44a0be41b13032581dae8b2f8c2e3e524
tatasharath <tatasharath@gmail.com>
2024-01-23 23:35:54 +0530

    Launch-ec2

--- Roboshop/launch-ec2.sh
+++ Roboshop/launch-ec2.sh
@@ -1,15 +1,15 @@
 #!/bin/bash 
 
-COMPONENT=$1 
+COMPONENT=$1
 ENV=$2
 HOSTEDZONEID="Z074928892F9CHCZSJQ4"
 INSTANCE_TYPE="t3.micro"
  
-if [ -z $1 ] || [ -z $2 ]  ; then 
-    echo -e "\e[31m COMPONENT NAME IS NEEDED \e[0m \n \t \t"
-    echo -e "\e[35m Ex Usage \e[0m \n\t\t $ bash launch-ec2.sh shipping"
-    exit 1
-fi 
+# if [ -z $1 ] || [ -z $2 ]  ; then 
+#     echo -e "\e[31m COMPONENT NAME IS NEEDED \e[0m \n \t \t"
+#     echo -e "\e[35m Ex Usage \e[0m \n\t\t $ bash launch-ec2.sh shipping"
+#     exit 1
+# fi 
 
 # AMI_ID="ami-0c1d144c8fdd8d690"
 AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7"| jq ".Images[].ImageId" | sed -e 's/"//g')" 
