1a8e5e8f943231f404e9c9fec4f582be8844f81e
tatasharath <tatasharath@gmail.com>
2024-01-24 23:36:40 +0530

    Launch-ec2

--- Roboshop/launch-ec2.sh
+++ Roboshop/launch-ec2.sh
@@ -20,13 +20,13 @@ create_ec2() {
     echo -e "****** Creating \e[35m ${COMPONENT} \e[0m Server Is In Progress **************"
     PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE}  --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}-${ENV}}]" | jq '.Instances[].PrivateIpAddress'| sed -e 's/"//g') 
 
-    echo -e "Private IP Address of the $COMPONENT-${ENV} is $PRIVATEIP \n\n"
+    echo -e "Private IP Address of the $COMPONENT-${ENV} is $PRIVATEIP \n"
     echo -e "Creating DNS Record of ${COMPONENT}: "
 
     sed -e "s/COMPONENT/${COMPONENT}-${ENV}/"  -e "s/IPADDRESS/${PRIVATEIP}/" route53.json  > /tmp/r53.json 
 
     aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/r53.json
-    echo -e "\e[36m **** Creating DNS Record for the $COMPONENT has completed **** \e[0m \n\n"
+    echo -e "\e[36m **** Creating DNS Record for the $COMPONENT has completed **** \e[0m \n"
 }
 
 if [ "$1" == "all" ]; then 
