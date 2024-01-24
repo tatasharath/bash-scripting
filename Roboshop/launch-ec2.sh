a08debc9f6da13ab741bb5be5af3f995dc06189e
tatasharath <tatasharath@gmail.com>
2024-01-25 00:39:57 +0530

    Launch-ec2 with ENV

--- Roboshop/launch-ec2.sh
+++ Roboshop/launch-ec2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 
-COMPONENT=$1
+COMPONENT=$1 
 ENV=$2
-HOSTEDZONEID="Z0776974D490R2S0TYGF"
+HOSTEDZONEID="Z074928892F9CHCZSJQ4"
 INSTANCE_TYPE="t3.micro"
  
-if [ -z $1 ]  ; then 
+if [ -z $1 ] || [ -z $2 ]  ; then 
     echo -e "\e[31m COMPONENT NAME IS NEEDED \e[0m \n \t \t"
     echo -e "\e[35m Ex Usage \e[0m \n\t\t $ bash launch-ec2.sh shipping"
     exit 1
@@ -17,21 +17,21 @@ SG_ID="$(aws ec2 describe-security-groups  --filters Name=group-name,Values=b55-
 
 create_ec2() {
 
-    echo -e "****** Creating \e[35m ${COMPONENT} \e[0m Server Is In Progress **************"
-    PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE}  --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress'| sed -e 's/"//g') 
+    echo -e "****** Creating \e[35m ${COMPONENT} \e[0m Server Is In Progress ************** "
+    PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE}  --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}-${ENV}}]" | jq '.Instances[].PrivateIpAddress'| sed -e 's/"//g') 
 
-    echo -e "Private IP Address of the $COMPONENT is $PRIVATEIP \n"
+    echo -e "Private IP Address of the $COMPONENT-${ENV} is $PRIVATEIP \n\n"
     echo -e "Creating DNS Record of ${COMPONENT}: "
 
-    sed -e "s/COMPONENT/${COMPONENT}/"  -e "s/IPADDRESS/${PRIVATEIP}/" route53.json  > /tmp/r53.json 
+    sed -e "s/COMPONENT/${COMPONENT}-${ENV}/"  -e "s/IPADDRESS/${PRIVATEIP}/" route53.json  > /tmp/r53.json 
 
     aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/r53.json
-    echo -e "\e[36m **** Creating DNS Record for the $COMPONENT has completed **** \e[0m \n"
+    echo -e "\e[36m **** Creating DNS Record for the $COMPONENT has completed **** \e[0m \n\n"
 }
 
 if [ "$1" == "all" ]; then 
 
-    for component in mongodb catalogue cart user shipping frontend payment mysql redis rabbitmq ; do 
+    for component in mongodb catalogue cart user shipping frontend payment mysql redis rabbitmg; do 
         COMPONENT=$component 
         create_ec2
     done 
